9ff15cc3034b1630f6e633930a0ce79027afcef6
Jake Thomas <jmaxt12@gmail.com>
2018-12-07 09:25:42 -0700

    First commit

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.sqlite3' }, // change this if you want a different name for the database
+    connection: { filename: './data/auth.sqlite3' }, // change this if you want a different name for the database
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
-      directory: './database/migrations',
+      directory: './data/migrations',
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
